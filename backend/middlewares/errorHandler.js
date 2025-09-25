c1b6a814a6f513c4e45a319b0fbce063ef6b00db
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-25 12:48:30 +0200

    feat: add Error Handler middleware

---
+++ backend/middlewares/errorHandler.js
@@ -0,0 +1,48 @@
+const {
+  PrismaPrismaClientKnownRequestError,
+} = require("@prisma/client/runtime");
+const languages = require("@utils/language");
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err.stack); // Log all errs for debugging purposes
+
+  const languageKey = req.get("Accept-Language")?.split("-")[0] || "sr";
+  const type = languages[languageKey];
+
+  let statusErrCode = err.status || 500;
+  let statusErrMessage = type.general_server_err;
+
+  // handle validation errs
+  if (err.name === "ValidationError") {
+    statusErrCode = 400;
+    statusErrCode = type[err.message] || err.message; // Fallback option just in case if it can't find the right language key from languages module
+  }
+
+  // handle Prisma specific errs
+  if (err instanceof PrismaPrismaClientKnownRequestError) {
+    // switch based on err code
+    switch (err.code) {
+      case "P2002":
+        statusErrCode = 409;
+        statusErrMessage = type.conflict_server_err;
+        break;
+
+      case "P2025":
+        statusErrCode = 404;
+        statusErrMessage = type.request_server_err;
+        break;
+      default:
+        statusErrCode = 500;
+        statusErrMessage = type.general_server_err;
+        break;
+    }
+  }
+  res.status(statusErrCode).json({
+    success: false,
+    statusErrMessage,
+  });
+};
+
+module.exports = {
+  errorHandler,
+};
