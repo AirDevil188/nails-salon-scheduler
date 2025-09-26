737ecb037c9f9550220f8d2969c096121a15aebe
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-26 12:27:54 +0200

    feat: add validateInvitation middleware

--- backend/controllers/invitationController.js
+++ backend/controllers/invitationController.js
@@ -1,6 +1,7 @@
 const db = require("@db/query");
 const { generateRefreshToken } = require("@utils/utils");
 const { addHours } = require("date-fns/addHours");
+const crypto = require("crypto");
 
 const generateInvitation = async (req, res, next) => {
   const { email } = req.body;
@@ -45,6 +46,47 @@ const generateInvitation = async (req, res, next) => {
   }
 };
 
+const validateInvitation = async (req, res, next) => {
+  // get the token from params from URL
+  const now = new Date();
+  const { token } = req.params;
+
+  let invitation;
+  try {
+    // find the invitation based on the token
+    invitation = await db.findInvitationByToken(token);
+    // check if the invitation is not found OR expired
+    if (!invitation || invitation.expiresAt < now) {
+      // if the invitation was found but expired invalidate/delete it
+      if (invitation && invitation.expiresAt < now) {
+        await db.invalidateInvitation(invitation);
+        console.log("Deleted expired invitation");
+      }
+      const error = new Error("authorization_err");
+      error.status = 401;
+      return next(error);
+    }
+
+    // if it is found and not expired
+
+    // generate random 6code
+    const randomCode = crypto.randomInt(100000, 999999 + 1);
+    // create authorization code that will be tied to this token in db
+    const updatedInvitation = db.createInvitationCode(
+      invitation.token,
+      randomCode
+    );
+    return res.status(200).json({
+      success: true,
+      message: "Invitation Token successfully validated",
+      code: updatedInvitation.code,
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
 module.exports = {
   generateInvitation,
+  validateInvitation,
 };
