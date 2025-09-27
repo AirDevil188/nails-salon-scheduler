490aa95c2dea21333555a301aa5ad1b2df06fe8f
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-27 15:18:41 +0200

    feat: add update invitation status to code_verified if the code is good

--- backend/controllers/invitationController.js
+++ backend/controllers/invitationController.js
@@ -107,6 +107,8 @@ const verifyInvitationCode = async (req, res, next) => {
       return next(error);
     }
 
+    // change the status of the invitation to the code_verified
+    await db.updateInvitationStatus(token, "code_verified");
     res.status(200).json({
       success: true,
       message: "Code verified successfully",
