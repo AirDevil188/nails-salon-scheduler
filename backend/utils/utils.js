003e134175f0dc5f02fd43b14e1559c6f1440c35
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-28 07:46:44 +0200

    chore: fix redudant signRefreshToken removed email, first and last name from payload

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -8,29 +8,14 @@ const verifyHash = async (value, hashedValue) => {
   return await bcrypt.compare(value, hashedValue);
 };
 
-const signToken = async (credentials) => {
+const signToken = async (credentials, expiresIn) => {
   const payload = {
     id: credentials.id,
-    email: credentials.email,
     role: credentials.role,
-    first_name: credentials.first_name,
-    last_name: credentials.last_name,
   };
   return jsonwebtoken.sign(payload, process.env.JWT_SECRET, {
     algorithm: "HS256",
-    expiresIn: "15m",
-  });
-};
-
-const signRefreshToken = async (credentials) => {
-  const payload = {
-    id: credentials.id,
-    email: credentials.email,
-  };
-
-  return jsonwebtoken.sign(payload, process.env.JWT_SECRET, {
-    algorithm: "HS256",
-    expiresIn: "1w",
+    expiresIn: expiresIn,
   });
 };
 
