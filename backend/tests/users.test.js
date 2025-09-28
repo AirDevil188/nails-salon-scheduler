c20704efd2ca137aec455c5f69ad8c3834aa58cb
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-28 16:40:29 +0200

    feat: new test wrong email  401

--- backend/tests/users.test.js
+++ backend/tests/users.test.js
@@ -74,4 +74,11 @@ describe("POST /users/sign-in", () => {
       .send({ email: testUser.email, password: "WrongPassword123!" })
       .expect(401);
   });
+
+  test("should give error 401 if the email is non existent", async () => {
+    await request(app)
+      .post("/users/sign-in")
+      .send({ email: "wrong@email.com", password: testUser.password })
+      .expect(401);
+  });
 });
