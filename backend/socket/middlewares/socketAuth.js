c320030491208fbec486a6a2eb37e1d376a1258f
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-04 01:01:52 +0200

    feat: add update user online status with event emit for it

--- backend/socket/middlewares/socketAuth.js
+++ backend/socket/middlewares/socketAuth.js
@@ -1,7 +1,11 @@
 const { createSocketAuthenticateError } = require("@utils/errorUtils");
 const { verifyToken } = require("@utils/utils");
+const db = require("@db/query.js");
+const { getIo } = require("@socketServices/socketManager");
 
 const authenticateSocketToken = async (socket, next) => {
+  const io = getIo();
+
   // set the token from the socket handshake
   const token = socket.handshake.auth.token;
 
@@ -23,11 +27,20 @@ const authenticateSocketToken = async (socket, next) => {
       );
       return next(error);
     }
+
+    await db.updateUserOnlineStatus(user.id, true, new Date());
     // if the token is valid assign the token to the socket.user
     socket.user = user;
+
+    console.log(`Prisma: User ${user.id} status successfully set to ONLINE.`);
+
+    io.emit("user.online", { userId: user.id, timestamp: new Date() });
     return next();
   } catch (err) {
-    return err;
+    const error = new Error(
+      createSocketAuthenticateError("Server failed to verify token", 500)
+    );
+    return next(error);
   }
 };
 
