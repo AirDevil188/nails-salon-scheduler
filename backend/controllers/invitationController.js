92ff307a25001c6020ef32e5bd29eaa8deaca0d9
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-04 01:05:47 +0200

    feat: Add real-time user notification broadcasts in invitationController

--- backend/controllers/invitationController.js
+++ backend/controllers/invitationController.js
@@ -4,8 +4,10 @@ const { addHours } = require("date-fns/addHours");
 const crypto = require("crypto");
 const { addMinutes } = require("date-fns/addMinutes");
 const { languages } = require("@utils/language");
+const { getIo } = require("../socket/services/socketManager");
 
 const generateInvitation = async (req, res, next) => {
+  const io = getIo();
   const languageKey = req.get("Accept-Language")?.split("-")[0] || "sr";
   const type = languages[languageKey];
 
@@ -31,12 +33,23 @@ const generateInvitation = async (req, res, next) => {
         email,
         addHours(new Date(), 8)
       );
+      io.to("admin-dashboard").emit("admin:updatedInvitation", invitation);
+      console.log(
+        `Sent updated invitation alert to the 'admin-dashboard' room for ID: ${invitation.id}.`
+      );
     } else {
       // if the invitation is not found create a new one
       invitation = await db.createInvitation(
         generateRawInvitationToken,
         email,
         addHours(new Date(), 8)
+      );
+      io.to("admin-dashboard").emit("admin:createdInvitation", {
+        email: invitation.email,
+        id: invitation.id,
+      });
+      console.log(
+        `Sent new invitation creation alert to the 'admin-dashboard' room for email: ${invitation.email}.`
       );
     }
 
@@ -68,6 +81,13 @@ const validateInvitation = async (req, res, next) => {
       // if the invitation was found but expired invalidate/delete it
       if (invitation && invitation.expiresAt < now) {
         await db.invalidateInvitation(invitation);
+        io.to("admin-dashboard").emit("admin:invalidatedInvitation", {
+          email: invitation.email,
+          id: invitation.id,
+        });
+        console.log(
+          `Sent new invitation invalidation alert to the 'admin-dashboard' room for email: ${invitation.email}.`
+        );
         console.log("Deleted expired invitation");
       }
       const error = new Error("authorization_err");
@@ -128,6 +148,13 @@ const verifyInvitationCode = async (req, res, next) => {
 
     // change the status of the invitation to the code_verified
     await db.verifyInvitationStatus(token, "code_verified");
+    io.to("admin-dashboard").emit("admin: invitationVerified", {
+      email: validateCode.email,
+      id: validateCode.id,
+    });
+    console.log(
+      `Sent new invitation verified status alert to the 'admin-dashboard' room for email: ${validateCode.email}.`
+    );
     res.status(200).json({
       success: true,
       message: type.success_invitation_verification_code,
@@ -166,8 +193,16 @@ const resendVerificationCode = async (req, res, next) => {
       Number(randomCode),
       addMinutes(new Date(), 5)
     );
+
     // resend new verification code by email
     const emailSent = await sendVerificationCode(invitation.email, randomCode);
+    io.to("admin-dashboard").emit("admin:codeResend", {
+      email: invitation.email,
+      id: invitation.id,
+    });
+    console.log(
+      `Sent code resend alert to the 'admin-dashboard' room for email: ${invitation.email}.`
+    );
 
     if (!emailSent) {
       console.error(
