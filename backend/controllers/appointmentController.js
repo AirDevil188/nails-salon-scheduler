e0ba99f3d2d6d27ef31b8adc01588c3cb0226236
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-05 21:38:54 +0200

    feat: add getMyAppointments middleware

---
+++ backend/controllers/appointmentController.js
@@ -0,0 +1,38 @@
+const { body, validationResult } = require("express-validator");
+const db = require("@db/query");
+const { isUUID } = require("@utils/utils");
+const { getIo } = require("@socketServices/socketManager");
+
+const getMyAppointments = async (req, res, next) => {
+  const { id } = req.user;
+  // set default if not provided to req query
+  try {
+    const { status, limit = "25", page = "1", orderBy, timeScope } = req.query;
+
+    const safeLimit = parseInt(limit, 10) || 25;
+    const safePage = parseInt(page, 10) || 1;
+
+    const { appointments, totalCount } = await db.getUserAppointments({
+      id,
+      status,
+      limit: safeLimit,
+      page: safePage,
+      orderBy,
+      timeScope,
+    });
+
+    return res.status(200).json({
+      appointments: appointments,
+      limit: safeLimit,
+      page: safePage,
+      totalCount: totalCount,
+      totalPages: Math.ceil(totalCount / safeLimit),
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+module.exports = {
+  getMyAppointments,
+};
