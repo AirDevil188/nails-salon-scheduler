3bc4c252f23b2243a3e858def333d4a1f25a8110
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-05 22:24:18 +0200

    feat: calendar route

--- backend/routes/appointmentRouter.js
+++ backend/routes/appointmentRouter.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
+const { authenticate } = require("@middlewares/authenticate");
 
 const appointmentRouter = Router();
 
 const appointmentController = require("@controllers/appointmentController");
 
 appointmentRouter.get("/", appointmentController.getMyAppointments);
+
+appointmentRouter.get(
+  "/calendar",
+  authenticate,
+  appointmentController.getMonthlyAppointments
+);
 
 module.exports = appointmentRouter;
