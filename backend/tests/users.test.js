afa86361f031fa9a2da60c1dfc4376e2c4e3fa8e
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-05 17:38:20 +0200

    feat: 401 change password_don't_match test

--- backend/tests/users.test.js
+++ backend/tests/users.test.js
@@ -154,6 +154,17 @@ describe("POST /users/sign-up", () => {
     const res = await request(app).get("/users/profile").expect(401);
   });
 
+  test("should not allow the change of user password if it doesn't match with current password", async () => {
+    const res = await request(app)
+      .patch("/users/profile/change-password")
+      .send({
+        current_password: "Test1234!!",
+        new_password: "Kengur123!",
+      })
+      .set(`Authorization`, `Bearer ${accessToken}`)
+      .expect(401);
+  });
+
   test("should change user password", async () => {
     const res = await request(app)
       .patch("/users/profile/change-password")
