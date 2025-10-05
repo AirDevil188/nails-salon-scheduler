d457af66692c46d6a6d3e10b1d995416ea8fa3d8
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-05 13:30:24 +0200

    feat: add change pass route

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -13,10 +13,15 @@ userRouter.post("/sign-in", userController.signInUser);
 userRouter.post("/sign-up", checkInvitationStatus, userController.signUpUser);
 
 userRouter.get("/profile", authenticate, userController.getUserProfile);
+
 userRouter.delete("/profile", authenticate, userController.deleteProfile);
 // :TODO:
 // user route to update avatar
-// user route to change password
+userRouter.patch(
+  "/profile/change-password",
+  authenticate,
+  userController.changePassword
+);
 // user route for forgotten password
 
 module.exports = userRouter;
