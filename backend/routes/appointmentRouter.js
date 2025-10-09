b9647a6291a890550425280fbc45dd1af2088b76
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-09 23:32:30 +0200

    feat: add managePushToken middleware to all routes

--- backend/routes/appointmentRouter.js
+++ backend/routes/appointmentRouter.js
@@ -4,23 +4,30 @@ const { authenticate } = require("@middlewares/authenticate");
 const appointmentRouter = Router();
 
 const { languagePrefer } = require("@middlewares/language");
+const { managePushToken } = require("@middlewares/pushToken");
 const appointmentController = require("@controllers/appointmentController");
 
 appointmentRouter.get(
   "/",
   authenticate,
+  languagePrefer,
+  managePushToken,
   appointmentController.getMyAppointments
 );
 
 appointmentRouter.get(
   "/calendar",
   authenticate,
+  languagePrefer,
+  managePushToken,
   appointmentController.getMonthlyAppointments
 );
 
 appointmentRouter.get(
   "/:appointmentId",
   authenticate,
+  languagePrefer,
+  managePushToken,
   appointmentController.getAppointmentDetails
 );
 
