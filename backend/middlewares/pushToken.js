d7b7b06b953ae52b209866751e6044ec2850d4e5
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-09 23:07:01 +0200

    feat: add pushToken middleware

---
+++ backend/middlewares/pushToken.js
@@ -0,0 +1,25 @@
+const db = require("@db/query");
+
+const managePushToken = async (req, res, next) => {
+  const deviceToken = req.headers["x-push-token"];
+  const id = req?.user;
+
+  if (!deviceToken || !id) {
+    console.error(
+      "Skipping token upsert due to missing user ID or device token."
+    );
+
+    return next();
+  }
+
+  try {
+    await db.upsertPushToken(id, deviceToken);
+    return next();
+  } catch (err) {
+    console.error("Error during push token upsert:", err);
+  }
+};
+
+module.exports = {
+  managePushToken,
+};
