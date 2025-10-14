2fd104a66db7e6ce718dbc358bd7ab5f27c29862
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-14 11:39:08 +0200

    feat: add  useTranslation hook

---
+++ frontend/src/hooks/useTranslation.js
@@ -0,0 +1,26 @@
+import useAuthStore from "../stores/useAuthStore";
+import { translations } from "../utils/translations";
+
+const getTranslationKey = (lang) => {
+  return translations[lang] || translations.sr;
+};
+
+export const useTranslation = () => {
+  // access the language from zustand store
+  const preferredLanguage = useAuthStore((state) => state.preferredLanguage);
+
+  // get active translation key
+  const tKey = getTranslationKey(preferredLanguage);
+
+  const t = (key) => {
+    return tKey || key;
+  };
+
+  const setLanguage = useAuthStore((state) => state.setLanguage);
+
+  return {
+    t,
+    setLanguage,
+    currentLanguage: preferredLanguage,
+  };
+};
