1e834765594e5b62712a5d44499684d7bda7de3c
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-14 20:45:59 +0200

    feat: add font styling

--- frontend/src/components/AppText.jsx
+++ frontend/src/components/AppText.jsx
@@ -12,20 +12,25 @@ export const AppTextStyle = StyleSheet.create({
   defaultText: {
     fontSize: 16,
     color: "#333",
+    fontFamily: "Inter-Regular",
   },
   h1: {
     fontSize: 32,
     fontWeight: "bold",
     marginBottom: 8,
+    fontFamily: "Inter-Bold",
   },
   h2: {
     fontSize: 24,
     fontWeight: "600",
     marginBottom: 8,
+    fontFamily: "Inter-Regular",
   },
   h3: {
     fontSize: 18,
     fontWeight: "500",
     marginBottom: 6,
+    textAlign: "center",
+    fontFamily: "Inter-Medium",
   },
 });
