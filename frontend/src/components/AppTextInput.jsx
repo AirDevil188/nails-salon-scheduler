df3ad06bc76f267fbe665614c7a8f3596ddbb74d
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-14 20:46:32 +0200

    feat: add AppTextInput component styling

--- frontend/src/components/AppTextInput.jsx
+++ frontend/src/components/AppTextInput.jsx
@@ -1,5 +1,33 @@
 import { TextInput } from "react-native";
+import { StyleSheet } from "react-native";
+import { theme } from "../theme";
 
 export default function AppTextInput({ style, ...otherProps }) {
-  return <TextInput style={style} {...otherProps}></TextInput>;
+  return (
+    <TextInput
+      underlineColorAndroid={"transparent"}
+      style={[styles.input, style]}
+      {...otherProps}
+      multiline={false}
+    ></TextInput>
+  );
 }
+const styles = StyleSheet.create({
+  input: {
+    shadowColor: theme.colorLightGrey,
+    height: 50,
+    backgroundColor: theme.colorWhite,
+    borderRadius: 6,
+    padding: 20,
+    overflow: "hidden",
+    marginBottom: 10,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+
+    elevation: 5,
+  },
+});
