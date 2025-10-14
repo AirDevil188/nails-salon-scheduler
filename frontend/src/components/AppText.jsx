4a88e087d3033ca7e219813b39a76cc559da701b
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-14 16:16:56 +0200

    feat: add AppTextStyle obj

--- frontend/src/components/AppText.jsx
+++ frontend/src/components/AppText.jsx
@@ -1,9 +1,31 @@
-import { Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
 
 export default function AppText({ children, style, ...otherProps }) {
   return (
-    <Text style={style} {...otherProps}>
+    <Text style={(AppTextStyle.defaultText, style)} {...otherProps}>
       {children}
     </Text>
   );
 }
+
+export const AppTextStyle = StyleSheet.create({
+  defaultText: {
+    fontSize: 16,
+    color: "#333",
+  },
+  h1: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  h2: {
+    fontSize: 24,
+    fontWeight: "600",
+    marginBottom: 8,
+  },
+  h3: {
+    fontSize: 18,
+    fontWeight: "500",
+    marginBottom: 6,
+  },
+});
