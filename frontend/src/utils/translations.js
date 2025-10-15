9c2024d41bc4f4044bfd9762553921ab6b31da68
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-15 23:38:53 +0200

    feat: add translation util helper

---
+++ frontend/src/utils/translations.js
@@ -0,0 +1,56 @@
+export const translations = {
+  en: {
+    // public routes
+    // sign-in.jsx
+    signInHeading: "Login to your account",
+    emailPlaceholder: "Email",
+    singInButton: "Sign In",
+
+    // Invite Flow
+
+    // [token].jsx
+    spinnerText: "Verifying Token",
+
+    //otp-input.jsx
+    mainHeading: "Check your Email",
+    informationText: "Enter the verification code sent to",
+    codeText: "Didn't get a code?",
+    resendLink: "resend",
+    verifyInvitationButton: "Verify Invitation",
+
+    // final-registration.jsx
+    signUpHeading: "Register your account",
+    firstNamePlaceholder: "First Name",
+    lastNamePlaceholder: "Last Name",
+    passwordPlaceholder: "Password",
+    confirmPasswordPlaceholder: "Confirm Password",
+    signUpButton: "Sign Up",
+  },
+  sr: {
+    // public routes
+    // sign-in.jsx
+    signInHeading: "Prijavite se na svoj nalog",
+    emailPlaceholder: "Mejl",
+    singInButton: "Prijavi se",
+
+    // Invite Flow
+
+    // [token].jsx
+    spinnerText: "Verifikacija Tokena",
+
+    //otp-input.jsx
+    mainHeading: "Proverite vašu mejl adresu",
+    informationText: "Unesite verifikacioni kod koji je poslat na",
+    codeText: "Niste dobili kod?",
+    resendLink: "pošalji ponovo",
+    verifyInvitationButton: "Potvrdi Pozivnicu",
+
+    // final-registration.jsx
+    signUpHeading: "Registrujte svoj nalog",
+    firstNamePlaceholder: "Ime",
+    lastNamePlaceholder: "Prezime",
+    passwordPlaceholder: "Lozinka",
+    confirmPasswordPlaceholder: "Potvrdi lozinku",
+    signUpButton: "Registruj se",
+  },
+};
