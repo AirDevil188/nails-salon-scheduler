ea968b7d03e0065a2ab19e31e37fac81666d9fa0
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-15 22:40:57 +0200

    changed look

--- frontend/app/sign-in.jsx
+++ frontend/app/sign-in.jsx
@@ -1,54 +1,53 @@
-import {
-  View,
-  StyleSheet,
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
-  StatusBar,
-} from "react-native";
-import AppText, { AppTextStyle } from "../src/components/AppText";
-import AppTextInput from "../src/components/AppTextInput";
+import { View, StyleSheet, ScrollView, StatusBar } from "react-native";
 import Logo from "../assets/logo.svg";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { KeyboardAwareScrollView } from "react-native-keyboard-controller";
 import AppButton from "../src/components/AppButton";
+import AppTextInput from "../src/components/AppTextInput";
+import AppText, { AppTextStyle } from "../src/components/AppText";
 import { theme } from "../src/theme";
+import { useTranslation } from "../src/hooks/useTranslation";
 
 export default function SignIn() {
+  const { t, currentLanguage } = useTranslation();
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={styles.container}
-    >
-      <StatusBar barStyle="dark-content" />
-      <ScrollView
-        contentContainerStyle={styles.scrollContent}
-        keyboardShouldPersistTaps="handled"
+    <SafeAreaView style={{ flex: 1 }}>
+      <KeyboardAwareScrollView
+        style={styles.container}
+        keyboardShouldPersistTaps={"handled"}
       >
-        <View style={styles.logoAndHeadingContainer}>
-          <Logo width={200} height={200} />
-        </View>
-        <View style={styles.inputContainer}>
-          <AppText style={[AppTextStyle.h1, styles.logoHeading]}>
-            Login to your account
-          </AppText>
-          <AppTextInput
-            placeholder="Email"
-            style={styles.input}
-            placeholderTextColor={theme.colorLightGrey}
-            keyboardType={"email-address"} // 💡 Best practice for email
-            autoCapitalize="none"
-          ></AppTextInput>
-          <AppTextInput
-            placeholder="Password"
-            style={styles.input}
-            inputMode={"password"}
-            placeholderTextColor={theme.colorLightGrey}
-            secureTextEntry={true}
-            autoCapitalize="none"
-          ></AppTextInput>
-          <AppButton>Sign in</AppButton>
-        </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
+        <StatusBar barStyle="dark-content" />
+        <ScrollView
+          contentContainerStyle={styles.scrollContent}
+          keyboardShouldPersistTaps="handled"
+        >
+          <View style={styles.inputContainer}>
+            <AppText style={[AppTextStyle.h1, styles.logoHeading]}>
+              {t("signInHeading")}
+            </AppText>
+            <AppTextInput
+              placeholder={t("emailPlaceholder")}
+              style={styles.input}
+              placeholderTextColor={theme.colorLightGrey}
+              keyboardType={"email-address"}
+              autoCapitalize="none"
+            ></AppTextInput>
+            <AppTextInput
+              placeholder={t("passwordPlaceholder")}
+              style={styles.input}
+              inputMode={"password"}
+              placeholderTextColor={theme.colorLightGrey}
+              secureTextEntry={true}
+              autoCapitalize="none"
+            ></AppTextInput>
+            <AppButton>{t("singInButton")}</AppButton>
+          </View>
+          <View style={styles.logoAndHeadingContainer}>
+            <Logo width={300} height={300} />
+          </View>
+        </ScrollView>
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -61,18 +60,19 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
   },
   logoAndHeadingContainer: {
-    // marginBottom: 30,
     alignItems: "center",
-    flex: 0,
+    flex: 1,
+    justifyContent: "center",
   },
   logoHeading: {
     fontSize: 24,
+    fontFamily: "Inter-Bold",
   },
   inputContainer: {
     margin: 10,
     paddingHorizontal: 15,
-    paddingVertical: 75,
-    flex: 0,
-    gap: 5,
+    paddingVertical: 10,
+    flex: 1,
+    gap: 10,
   },
 });
