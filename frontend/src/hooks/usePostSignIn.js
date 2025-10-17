ed2cfd46b41502a0490a416f3c635bbf94e01dc7
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-18 00:10:39 +0200

    feat: add postSignIn hook

---
+++ frontend/src/hooks/usePostSignIn.js
@@ -0,0 +1,33 @@
+import { useMutation } from "@tanstack/react-query";
+import api from "../utils/axiosInstance";
+import useAuthStore from "../stores/useAuthStore";
+import { router } from "expo-router";
+import { saveToken } from "../utils/secureStore";
+
+export default function usePostSignIn() {
+  return useMutation({
+    mutationFn: async (payload) => {
+      const { email, password } = payload;
+
+      const response = await api.post("/api/users/sign-in", {
+        email: email,
+        password: password,
+      });
+
+      return response.data;
+    },
+    onSuccess: async (data) => {
+      const { login } = useAuthStore.getState();
+
+      if (data?.accessToken && data?.refreshToken && data?.userInfo) {
+        // save tokens to expo-secure-storage
+
+        await saveToken("accessToken", data.accessToken);
+        await saveToken("refreshToken", data.refreshToken);
+        // save tokens and userInfo to the auth store
+        login(data.accessToken, data.refreshToken, data.userInfo);
+        return router.replace("/");
+      }
+    },
+  });
+}
