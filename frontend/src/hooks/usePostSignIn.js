7b9f487ec520d5a8c6d6d70f39fb7986377101ef
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-18 23:35:48 +0200

    feat: add save userInfo in the expo-secure-store

--- frontend/src/hooks/usePostSignIn.js
+++ frontend/src/hooks/usePostSignIn.js
@@ -2,7 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import api from "../utils/axiosInstance";
 import useAuthStore from "../stores/useAuthStore";
 import { router } from "expo-router";
-import { saveToken } from "../utils/secureStore";
+import { saveToken, saveUserInfo } from "../utils/secureStore";
 
 export default function usePostSignIn() {
   return useMutation({
@@ -24,6 +24,7 @@ export default function usePostSignIn() {
 
         await saveToken("accessToken", data.accessToken);
         await saveToken("refreshToken", data.refreshToken);
+        await saveUserInfo("userInfo", data.userInfo);
         // save tokens and userInfo to the auth store
         login(data.accessToken, data.refreshToken, data.userInfo);
         return router.replace("/");
