3e79b51118f8d38c71cc198515953437460af0ce
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-18 23:38:35 +0200

    feat: add secureStore helper func

--- frontend/src/utils/secureStore.js
+++ frontend/src/utils/secureStore.js
@@ -1,5 +1,30 @@
 import * as SecureStore from "expo-secure-store";
 
+const secureStoreKeys = ["accessToken", "refreshToken", "userInfo"];
 export async function saveToken(key, value) {
   await SecureStore.setItemAsync(key, value);
+}
+
+export async function getToken(key) {
+  return await SecureStore.getItemAsync(key);
+}
+
+export async function saveUserInfo(key, value) {
+  await SecureStore.setItemAsync(key, JSON.stringify(value));
+}
+
+export async function clearSecureStorage() {
+  console.log("Clearing all session keys from SecureStore...");
+
+  const removalPromises = secureStoreKeys.map((key) => {
+    console.log(`Removing key: ${key}`);
+    return SecureStore.deleteItemAsync(key);
+  });
+
+  try {
+    await Promise.all(removalPromises);
+    console.log("Successfully cleared all session data from SecureStore.");
+  } catch (error) {
+    console.error("Error clearing SecureStore:", error);
+  }
 }
