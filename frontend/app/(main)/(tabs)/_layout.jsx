a675b8ae8436a915ed5199ec491f121ff54d8c53
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-24 00:06:13 +0200

    feat: add calendar icon in right header changed color

--- frontend/app/(main)/(tabs)/_layout.jsx
+++ frontend/app/(main)/(tabs)/_layout.jsx
@@ -2,13 +2,17 @@ import { Link, Tabs } from "expo-router";
 
 import { Pressable } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 import { theme } from "../../../src/theme";
 import DrawerToggle from "../../../src/components/DrawerToggle";
+import { useState } from "react";
+import useLayoutStore from "../../../src/stores/useLayoutStore";
 
 export default function TabsLayout() {
-  // mount listener for the profile
-  // useSocketProfileLister();
+  const isListView = useLayoutStore((state) => state.isListView);
+  const toggleListView = useLayoutStore((state) => state.toggleListView);
+
   return (
     <Tabs
       screenOptions={{ headerShown: true, headerLeft: () => <DrawerToggle /> }}
@@ -20,13 +24,39 @@ export default function TabsLayout() {
           headerRight: () => (
             <Link href={"/edit-profile"} asChild>
               <Pressable style={{ marginRight: 18 }}>
-                <MaterialIcons name="edit" size={24} color="black" />
+                <MaterialIcons
+                  name="edit"
+                  size={24}
+                  color={theme.colorDarkPink}
+                />
               </Pressable>
             </Link>
           ),
         }}
       />
-      <Tabs.Screen name="index" options={{ title: "Calendar" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Calendar",
+          headerRight: () => (
+            <Pressable style={{ marginRight: 18 }} onPress={toggleListView}>
+              {isListView ? (
+                <FontAwesome
+                  name="calendar"
+                  size={24}
+                  color={theme.colorDarkPink}
+                />
+              ) : (
+                <MaterialIcons
+                  name="format-list-bulleted"
+                  size={24}
+                  color={theme.colorDarkPink}
+                />
+              )}
+            </Pressable>
+          ),
+        }}
+      />
       <Tabs.Screen name="notes" options={{ title: "Notes" }} />
     </Tabs>
   );
