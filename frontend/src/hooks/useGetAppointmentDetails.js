6e473a6d094fe81b0743c295e9267f9292c0a4a4
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-25 15:32:35 +0200

    feat: add useGetAppointmentDetails hook

---
+++ frontend/src/hooks/useGetAppointmentDetails.js
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuthStore from "../stores/useAuthStore";
+import { appointmentKeys } from "../utils/queryKeys";
+import api from "../utils/axiosInstance";
+
+export default function useGetAppointmentDetails(id) {
+  const { userInfo } = useAuthStore.getState();
+  const { role } = userInfo;
+  console.log(role);
+  const adminEndpoint = `/api/admin/appointments/${id}`;
+  const userEndpoint = `/api/appointments/${id}`;
+  return useQuery({
+    queryKey: appointmentKeys.detail(id),
+    queryFn: async () => {
+      if (role === "admin") {
+        console.warn("TRUE");
+        const response = await api.get(adminEndpoint);
+        console.log(response);
+        return response.data.appointment;
+      } else {
+        const response = await api.get(userEndpoint);
+        return response.data.appointment;
+      }
+    },
+    enabled: !!id && !!role, // the query should run only if the id and role is available
+  });
+}
