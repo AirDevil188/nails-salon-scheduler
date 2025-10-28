f095003357d008096b50d20122834dcdd14b33b9
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-28 11:33:05 +0100

    feat: add delete btn func

--- frontend/src/components/AppointmentHeaderEdit.jsx
+++ frontend/src/components/AppointmentHeaderEdit.jsx
@@ -1,12 +1,33 @@
 // src/components/AppointmentEditHeader.jsx
-import { View, TouchableOpacity } from "react-native";
-import { Link, useLocalSearchParams } from "expo-router"; // 🔑 Use hook here
+import { View, TouchableOpacity, Alert } from "react-native";
+import { Link, router, useLocalSearchParams } from "expo-router"; // 🔑 Use hook here
 import { MaterialIcons } from "@expo/vector-icons";
 import { theme } from "../theme";
+import { useTranslation } from "../hooks/useTranslation";
+import api from "../utils/axiosInstance";
+import useDeleteAppointment from "../hooks/useDeleteAppointment";
 
 export function AppointmentEditHeader({ appointmentId }) {
+  const { t } = useTranslation();
+  const deleteAppointmentMutation = useDeleteAppointment();
   if (!appointmentId) return null;
 
+  const handleDelete = () => {
+    Alert.alert(t("appointmentAlert"), "", [
+      {
+        text: t("yesPlaceholder"),
+        onPress: async () => {
+          deleteAppointmentMutation.mutate(appointmentId, {
+            onSuccess: () => {
+              router.back();
+            },
+          });
+        },
+        style: "destructive",
+      },
+      { text: t("noPlaceholder"), style: "cancel" },
+    ]);
+  };
   return (
     <View
       style={{
@@ -29,11 +50,9 @@ export function AppointmentEditHeader({ appointmentId }) {
         </TouchableOpacity>
       </Link>
 
-      <Link href={"/edit-profile"} asChild>
-        <TouchableOpacity>
-          <MaterialIcons name="delete" size={24} color={theme.colorDarkPink} />
-        </TouchableOpacity>
-      </Link>
+      <TouchableOpacity onPress={handleDelete}>
+        <MaterialIcons name="delete" size={24} color={theme.colorDarkPink} />
+      </TouchableOpacity>
     </View>
   );
 }
