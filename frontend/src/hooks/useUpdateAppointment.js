06544bb4e8da48e35bbf291f0c89bb6093a7342e
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-28 10:09:19 +0100

    feat: add useUpdateAppointment hook

---
+++ frontend/src/hooks/useUpdateAppointment.js
@@ -0,0 +1,43 @@
+import useAuthStore from "../stores/useAuthStore";
+import api from "../utils/axiosInstance";
+import { appointmentKeys } from "../utils/queryKeys";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export default function useUpdateAppointment() {
+  const queryClient = useQueryClient();
+  const { userInfo } = useAuthStore.getState();
+  const role = userInfo?.role;
+
+  return useMutation({
+    mutationKey: appointmentKeys.update,
+
+    mutationFn: async (payload) => {
+      const { id, ...updatedFields } = payload;
+      console.error(payload);
+
+      if (role === "admin") {
+        const response = await api.patch(
+          `/api/admin/appointments/${id}`,
+          updatedFields
+        );
+        console.error(response);
+        return response.data;
+      }
+    },
+    onSuccess: (updatedAppointmentData, variables) => {
+      const appointmentId = variables.id;
+
+      queryClient.invalidateQueries({
+        // Invalidate using the specific ID, allowing partial key match
+        queryKey: appointmentKeys.detail(appointmentId),
+      });
+      // main appointment list (which uses list keys) is refreshed.
+      queryClient.invalidateQueries({
+        queryKey: appointmentKeys.list(),
+      });
+    },
+    onError: (error) => {
+      console.error("Appointment update failed:", error);
+    },
+  });
+}
