acca53b02e5fdccc2236abac06d5d818212e2ecb
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-10-30 21:11:57 +0100

    feat: add socket listener for creating, updating deleting notes

--- frontend/src/hooks/useAdminSocketListener.js
+++ frontend/src/hooks/useAdminSocketListener.js
@@ -1,6 +1,6 @@
 import { useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { appointmentKeys } from "../utils/queryKeys";
+import { appointmentKeys, noteKeys } from "../utils/queryKeys";
 import { socket } from "../utils/socket";
 
 export const useAdminSocketListener = (shouldListen) => {
@@ -8,6 +8,50 @@ export const useAdminSocketListener = (shouldListen) => {
 
   useEffect(() => {
     if (!shouldListen || !socket) return;
+
+    const handleNoteData = (note) => {
+      if (!note || !note.id) return;
+
+      // Update detail cache
+      queryClient.setQueryData(noteKeys.detail(note.id, "admin"), note);
+
+      // Update list cache
+      queryClient.setQueryData(noteKeys.list(), (oldData) => {
+        if (!oldData || !Array.isArray(oldData)) return [note];
+        const exists = oldData.some((a) => a.id === note.id); // check if the note is already in the cache
+
+        return exists
+          ? oldData.map((a) => (a.id === note.id ? note : a))
+          : [note, ...oldData];
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: noteKeys.all,
+        exact: false,
+      });
+
+      console.log(`[Socket Admin] Note created/updated ID: ${note.id}`);
+    };
+
+    const handleNoteDeleted = (noteId) => {
+      if (!noteId) return;
+
+      queryClient.removeQueries({
+        queryKey: noteKeys.detail(noteId, "admin"),
+      });
+
+      queryClient.setQueryData(
+        noteKeys.list(),
+        (oldData) => oldData?.filter((a) => a.id !== noteId) || []
+      );
+
+      queryClient.invalidateQueries({
+        queryKey: noteKeys.all,
+        exact: false,
+      });
+
+      console.log(`[Socket Admin] Note deleted ID: ${noteId}`);
+    };
 
     const handleAppointmentData = (appointment) => {
       if (!appointment || !appointment.id) return;
@@ -34,6 +78,7 @@ export const useAdminSocketListener = (shouldListen) => {
         queryKey: appointmentKeys.all,
         exact: false,
       });
+
       console.log(
         `[Socket Admin] Appointment created/updated ID: ${appointment.id}`
       );
@@ -57,19 +102,27 @@ export const useAdminSocketListener = (shouldListen) => {
         queryKey: appointmentKeys.all,
         exact: false,
       });
+
       console.log(`[Socket Admin] Appointment deleted ID: ${appointmentId}`);
     };
 
     // Attach listeners
+    socket.on("admin:noteCreated", handleNoteData);
+    socket.on("admin:noteUpdated", handleNoteData);
+    socket.on("admin:noteDeleted", handleNoteDeleted);
     socket.on("admin:appointment:created", handleAppointmentData);
     socket.on("admin:appointment:updated", handleAppointmentData);
     socket.on("admin:appointment:deleted", handleAppointmentDeleted);
 
     // Cleanup
     return () => {
+      socket.off("admin:noteCreated", handleNoteData);
+      socket.off("admin:noteUpdated", handleNoteData);
+      socket.off("admin:noteDeleted", handleNoteDeleted);
       socket.off("admin:appointment:created", handleAppointmentData);
       socket.off("admin:appointment:updated", handleAppointmentData);
       socket.off("admin:appointment:deleted", handleAppointmentDeleted);
+
       console.log("[Socket Admin] Listener cleanup done");
     };
   }, [queryClient, shouldListen]);
